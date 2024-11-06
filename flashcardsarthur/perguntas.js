4d410f9d4bec485366d517c714dd4d6cbfe82f77
arthurferri03 <167265178+arthurferri03@users.noreply.github.com>
2024-11-05 22:34:19 -0300

    Add files via upload

---
+++ flashcardsarthur/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'QUÍMICA',
+    'Quantas ligações um carbono deve ter?',
+    'UM CARBONO TEM QUE TEM 4 LIGAÇÕES.'
+)
+
+criaCartao(
+    'PORTUGUÊS',
+    'Oque e uma escrita formal',
+    'A linguagem formal é marcada pelo uso da norma culta.'
+)
+
+criaCartao(
+    'MATEMÁTICA',
+    'O que é uma equação do 1º grau?',
+    'A equação do 1º grau é uma equação que possui incógnita com grau 1.'
+)
+
+criaCartao(
+    'LINGUA INGLESA',
+    'Como falar "olá preciso de uma informção" em Inglês?',
+    'Hi I need some information.'
+)
